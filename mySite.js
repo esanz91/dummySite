2c8e40ce7e2e8f3bde893b66c3b477f2f90ba21b
Elizabeth Sanz <esanz91@gmail.com>
2015-04-18 00:17:04 -0700

    fixed typo error in mySite.js and updated tech-course with correct information

--- mySite.js
+++ mySite.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var handlebars = require('express3-handlebars').create({
     defaultLayout:'main',
-    helper: {
+    helpers: {
         section: function(name, options){
             if(!this._sections) this._sections = {};
             this._sections[name] = options.fn(this);
