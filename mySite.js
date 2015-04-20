38c91408594db647b2e1d50b812203632b12eb8e
Elizabeth Sanz <esanz91@gmail.com>
2015-04-20 16:49:45 -0700

    added a form and bootstrap

--- mySite.js
+++ mySite.js
@@ -28,6 +28,9 @@ app.set('port', process.env.PORT || 3000);
 //define route for static files
 app.use(express.static(__dirname + '/public'));
 
+//add body-parse middleware
+app.use(require('body-parser')());
+
 //set up global page testing
 app.use(function(req, res, next){
     //if the expression is true, then res.locals.showTests = true
@@ -80,6 +83,31 @@ app.get('/data/tech-course', function(req, res){
         cDescription: 'JavaScript for Beginners'
     });
 });
+
+
+app.get('/newsletter', function(req, res){
+   res.render('newsletter', {csrf: 'CRSF token goes here'})
+});
+
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+
+        //print data recieved from form
+        console.log('Form (from querystring): ' + req.query.form); //should print out newsletter as defined in newsletter.handlebars
+        console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+        console.log('Name (from visible form field): ' + req.body.name);
+        console.log('Email (from visible form field): ' + req.body.email);
+
+        //usually we would save the information into the database at this point
+
+        //redirect
+        res.redirect(303, '/thank-you');
+    }
+})
 
 //mount middleware (userRoute)
 app.use('/user', userRoute);
